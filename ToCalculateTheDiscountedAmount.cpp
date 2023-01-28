070f3feaa7eae91996ea466a56e14632bd6cc0ec
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-01-29 04:11:04 +0530

    Add files via upload

--- ToCalculateTheDiscountedAmount.cpp
+++ ToCalculateTheDiscountedAmount.cpp
@@ -2,32 +2,32 @@
 //  ToCalculateTheDiscountedAmount.cpp
 //  Conditional Statements
 //
-//  Created by Chaitanya Gadkari on 23/01/23.
+//  Created by Chaitanya Gadkari on 25/01/23.
 //
 
 #include<iostream>
-#include<cmath>
-
 using namespace std;
 
-int main() {
-    int amount = 0;
-    float disAmount;
-    cout<<"Enter your bill amount ";
-    cin>>amount;
-    if (amount > 5000) {
-        disAmount = amount - amount*20/100.0;
-        cout<<"Your Discounted Bill amount after applying 20% discount is"<<disAmount<<endl;
+int main()
+{
+    float billAmount;
+    float discount=0.0;
+
+    cout<<"Enter Bill Amount:";
+    cin>>billAmount;
+        
+    if(billAmount>=5000) {
+        discount=billAmount*20/100;
     }
-    else {
-        if (amount >= 2000 && amount < 5000) {
-            disAmount = amount - amount*10/100.0;
-            cout<<"Your Discounted Bill amount after applying 10% discount is "<<disAmount<<endl;
-        }
-        else {
-            disAmount = amount - amount*5/100.0;
-            cout<<"Your Discounted Bill amount after applying 5% discount is "<<disAmount<<endl;
-        }
+    
+    else if(billAmount>=1000 && billAmount<5000) {
+        discount=billAmount*10/100;
+        billAmount=billAmount-discount;
     }
+    cout<<"The Bill Amount is:"<<billAmount<<endl;
+    cout<<"The Discount is :"<<discount<<endl;
+    cout<<"The Discounted Amount is:"<<billAmount - discount<<endl;
+        
     return 0;
+        
 }
